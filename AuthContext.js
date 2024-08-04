73e1aa7635f36d109931251713a54a48efae536a
Shashikant Patil <shashikant@enhanzed.com>
2024-08-05 01:23:42 +0530

    Added UserData

--- AuthContext.js
+++ AuthContext.js
@@ -1,16 +1,18 @@
 // contexts/AuthContext.js
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 // Create the context
 export const AuthContext = React.createContext();
 
 // Create a provider component
-export const AuthProvider = ({ children }) => {
+export const AuthProvider = ({children}) => {
   const [userRole, setUserRole] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   return (
-    <AuthContext.Provider value={{ userRole, setUserRole }}>
+    <AuthContext.Provider
+      value={{userRole, setUserRole, userData, setUserData}}>
       {children}
     </AuthContext.Provider>
   );
